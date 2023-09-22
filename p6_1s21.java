34a25f285c532371289374d2ef0895d59003abc8
BrianRubenGonzalezBlanco <144179421+BrianRubenGonzalezBlanco@users.noreply.github.com>
2023-09-22 12:00:12 -0600

    Pacticas 5y 6
    Gonzalez Blanco Brian Ruben 1s21

---
+++ p6_1s21.java
@@ -0,0 +1,11 @@
+class p6_1s21{
+	public static void main(String[] args) {
+		String nombre = "brian";
+		int edad = 21;
+		System.out.printl("El valor para nomre es" + brian);
+		System.out.printl("la edad es" + edad);
+		System.out.printl ("puedeo proseguir programando");
+		
+	}
+
+}
